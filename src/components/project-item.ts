45db6ebf2b423e7e1accfca15bdb88cbd5297b17
Connor Cowan <ccowan003@comcast.net>
2023-04-08 15:36:20 -0700

    Refactor with es Modules

--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -1,51 +1,49 @@
-/// <reference path="base-component.ts"/>
-/// <reference path="../decorators/autobind.ts" />
-/// <reference path="../models/project-model.ts" />
-/// <reference path="../interfaces/drag-drop.ts" />
+import { Draggable } from "../interfaces/drag-drop.js";
+import Component from "./base-component.js";
+import { Project } from "../models/project-model.js";
+import { autobind } from "../decorators/autobind.js";
 
 //ProjectItem Class
-namespace App {
-  export class ProjectItem
-    extends Component<HTMLUListElement, HTMLLIElement>
-    implements Draggable
-  {
-    private project: Project;
 
-    get persons() {
-      if (this.project.people === 1) {
-        return "1 person";
-      } else {
-        return `${this.project.people} persons`;
-      }
-    }
+export class ProjectItem
+  extends Component<HTMLUListElement, HTMLLIElement>
+  implements Draggable
+{
+  private project: Project;
 
-    constructor(hostId: string, project: Project) {
-      super("single-project", hostId, false, project.id);
-      this.project = project;
+  get persons() {
+    if (this.project.people === 1) {
+      return "1 person";
+    } else {
+      return `${this.project.people} persons`;
+    }
+  }
 
-      this.configure();
-      this.renderContent();
-    }
-    @autobind
-    dragStartHandler(event: DragEvent) {
-      event.dataTransfer!.setData("text/plain", this.project.id);
-      event.dataTransfer!.effectAllowed = "move";
-    }
+  constructor(hostId: string, project: Project) {
+    super("single-project", hostId, false, project.id);
+    this.project = project;
 
-    dragEndHandler(_: DragEvent) {
-      console.log("DragEnd");
-    }
+    this.configure();
+    this.renderContent();
+  }
+  @autobind
+  dragStartHandler(event: DragEvent) {
+    event.dataTransfer!.setData("text/plain", this.project.id);
+    event.dataTransfer!.effectAllowed = "move";
+  }
 
-    configure() {
-      this.element.addEventListener("dragstart", this.dragStartHandler);
-      this.element.addEventListener("dragend", this.dragEndHandler); //HAD dragstart here got no id
-    }
+  dragEndHandler(_: DragEvent) {
+    console.log("DragEnd");
+  }
 
-    renderContent() {
-      this.element.querySelector("h2")!.textContent = this.project.title;
-      this.element.querySelector("h3")!.textContent =
-        this.persons + " assigned";
-      this.element.querySelector("p")!.textContent = this.project.description;
-    }
+  configure() {
+    this.element.addEventListener("dragstart", this.dragStartHandler);
+    this.element.addEventListener("dragend", this.dragEndHandler); //HAD dragstart here got no id
+  }
+
+  renderContent() {
+    this.element.querySelector("h2")!.textContent = this.project.title;
+    this.element.querySelector("h3")!.textContent = this.persons + " assigned";
+    this.element.querySelector("p")!.textContent = this.project.description;
   }
 }
